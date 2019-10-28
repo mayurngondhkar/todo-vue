8a8851819a5f500a0ac29f47c06cc13562297c1c
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-29 01:18:22 +1100

    Add mutation types for todos, steps, tasks and statuses

---
+++ src/store/mutationTypes.ts
@@ -0,0 +1,19 @@
+export const GET_TODOS = 'getTodos';
+export const ADD_TODO = 'addTodo';
+export const UPDATE_TODO = 'updateTodo';
+export const DELETE_TODO = 'deleteTodo';
+
+export const GET_STEPS = 'getSteps';
+export const ADD_STEP = 'addStep';
+export const UPDATE_STEPS = 'updateStep';
+export const DELETE_STEPS = 'deleteStep';
+
+export const GET_TASKS = 'getTasks';
+export const ADD_TASKS = 'addTask';
+export const UPDATE_TASKS = 'updateTask';
+export const DELETE_TASKS = 'deleteTask';
+
+export const GET_STATUSES = 'getStatuses';
+export const ADD_STATUSES = 'addStatus';
+export const UPDATE_STATUSES = 'updateStatus';
+export const DELETE_STATUSES = 'deleteStatus';
