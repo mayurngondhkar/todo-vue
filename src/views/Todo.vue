cbd347f02ae5ae3d7c2ee50925258fa4377ee670
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-29 01:47:01 +1100

    Delete todolist
    Todo list will be listed in the todo component.
    View folder to hold files that will be in the routes.

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="Todo">
-    <TodoList msg="Welcome to the todo list" />
-  </div>
+<div>
+    Hello
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import TodoList from "@/components/TodoList.vue";
 
 export default {
   name: "todo",
-  components: {
-    TodoList
-  }
 };
 </script>
