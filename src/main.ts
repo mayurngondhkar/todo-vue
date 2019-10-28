fa5adf5598bbc6650103e1e6521f95bfad0867f8
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-29 01:37:39 +1100

    Add Bootstrap to entry point
    https://bootstrap-vue.js.org/docs/

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
